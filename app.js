7613c089aba6772f8ce06e6f234263a35f366d35
Aman Tyagi <amantyagi2k@gmail.com>
2022-08-07 17:10:55 +0530

    Heroku Hosting

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use("/:string", async (req, res) => {
 });
 app.use("/", listRouter);
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
